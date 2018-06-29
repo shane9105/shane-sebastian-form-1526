5b6bbd21c5357da85ed28b6bb4a7ad6ecb0e2596
SHANE SEBASTIAN <39057461+shane9105@users.noreply.github.com>
2018-06-29 13:26:47 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,70 @@
+// Shortcut for printing in JavaScript console
+
+
+// getting references;
+const main = document.querySelector(".main");
+const form = document.querySelector("form");
+const fullname = document.getElementById("fname");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+const pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; // email reg-exp
+
+
+const collectedData = {
+	fullName: null,
+	email: null,
+	message: null
+};
+
+const errors = {};
+
+function validateForm(ev) {
+
+	ev.preventDefault();
+
+	// validate full name
+	
+	if (fullname.value !== "") {
+		collectedData.fullName = fullname.value;
+		delete errors.ev;
+	} else {
+		errors.ev = "Full name is missing";
+	}
+
+
+
+	// validate email
+	
+	if (email.value !== "") {
+		if (pattern.test(email.value)) {
+			collectedData.email = email.value;
+			delete errors.em;
+		} else {
+			errors.em = "Email is invalid";
+		}
+	} else {
+		errors.em = "Email is missing";
+	}
+
+
+	// validate message
+	
+	if (message.value !== "") {
+		collectedData.message = message.value;
+		delete errors.msg;
+	} else {
+		errors.msg = "Message name is missing.";
+	}
+
+
+	// provide feedback or error-report
+	
+	if (Object.keys(errors).length === 0) {
+		console.log(collectedData);
+	} else {
+		console.log(errors)
+	}
+
+}
+
+form.addEventListener("submit", validateForm);
